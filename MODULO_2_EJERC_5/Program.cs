19c40acdfb49f4706582d843bd54b6e52a4e361d
cami63830 <cami63830@gmail.com>
2018-09-29 21:14:26 -0300

    si

--- MODULO_2_EJERC_5/Program.cs
+++ MODULO_2_EJERC_5/Program.cs
@@ -10,6 +10,18 @@ namespace MODULO_2_EJERC_5
     {
         static void Main(string[] args)
         {
+
+            //Tarea 5: Calcular el resultado de(20 + 5) % 6
+            int V7, V8, SU;
+            V7 = 20;
+            V8 = 0;
+            SU = 0;
+            SU = V7 + V8;
+            SU = SU % 6;
+            Console.WriteLine("Ejercicio 4: el resultado de (20 +5)%6= {0}", SU);
+            Console.ReadKey();
+
+
         }
     }
 }
